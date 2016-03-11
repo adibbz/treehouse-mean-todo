7ea457d83677f4cceaaa5465a2571fc22fcbaac6
Andrew Dibb <adibb@gannett.com>
2016-03-11 16:36:38 -0600

    Finished MEAN-todo App

--- app/app.js
+++ app/app.js
@@ -4,4 +4,7 @@ var angular = require('angular');
 
 angular.module('todoListApp', []);
 
-require('./scripts/controllers/main.js')
+require('./scripts/controllers/main.js');
+require('./scripts/controllers/todo.js');
+require('./scripts/directives/todo.js');
+require('./scripts/services/data.js');
--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var angular = require('angular');
+
 angular.module('todoListApp')
 .service('dataService', function($http, $q) {
   this.getTodos = function(cb) {
